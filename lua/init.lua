ce79763fae84800fe03d85b62737adcd03d2c527
vgitv <33827923+vgitv@users.noreply.github.com>
2025-01-29 13:46:17 +0100

    fix comments

--- lua/init.lua
+++ lua/init.lua
@@ -1,21 +1,23 @@
+-- Plugin initialisation.
 -- This init module must be run even if the setup function is not called so the plugin is usable with default options
 -- without the need of calling the setup function with empty arguments.
 
 local config = require "config"
 
----The main terminal background could be darker than the editor background
----@param opts table
-local get_term_bg = function(opts)
-    local factor = math.max(0, opts.factor)
+---Compute main terminal background color
+---@param factor number Factor to apply to red / green / blue parts of Normal bg color
+---@return string: Terminal background color in the form #XXXXXX
+local get_term_bg = function(factor)
+    factor = math.max(0, factor or 0.75)
 
     -- Try to guess a good background color for the main terminal window.
     local normal_bg = string.format("#%06x", vim.api.nvim_get_hl(0, { name = "Normal", create = false }).bg)
 
-    -- TODO math.max("FF", factor) if factor > 1
     local red = tonumber("0x" .. string.sub(normal_bg, 2, 3))
     local green = tonumber("0x" .. string.sub(normal_bg, 4, 5))
     local blue = tonumber("0x" .. string.sub(normal_bg, 6, 7))
 
+    -- if factor > 1, be sure not to exceed FF (=255)
     local hex_red = string.format("%02x", math.min(red * factor, 255))
     local hex_green = string.format("%02x", math.min(green * factor, 255))
     local hex_blue = string.format("%02x", math.min(blue * factor, 255))
@@ -23,9 +25,9 @@ local get_term_bg = function(opts)
     return "#" .. hex_red .. hex_green .. hex_blue
 end
 
----Initialisation function
+---Set terminal highlight group for later background color
 local set_term_hl = function()
-    local color = get_term_bg { factor = config.options.bg_color_factor }
+    local color = get_term_bg(config.options.bg_color_factor)
     vim.api.nvim_set_hl(0, "MainTerminalNormal", { bg = color })
 end
 
