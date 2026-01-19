849ad150566397c4ccb88bd2e034a95fd3de0e92
vgitv <33827923+vgitv@users.noreply.github.com>
2026-01-19 09:40:05 +0100

    open method

--- lua/term/window.lua
+++ lua/term/window.lua
@@ -1,16 +1,31 @@
 -- Window class-like table
 
+local utils = require "utils"
+
 ---@class Window
+---@field buf integer Buffer id
 ---@field id integer Window id
+---@field config table Window config
 local Window = {}
 
-function Window:new()
+---Window constructor
+---@param buf integer Buffer id
+function Window:new(buf)
     local window = {
+        buf = utils.get_buf(buf),
         id = nil,
+        config = nil,
     }
     setmetatable(window, self)
     self.__index = self
     return window
+end
+
+---Open the window
+---@param enter boolean Enter the window or not
+function Window:open(enter)
+    enter = enter or false
+    self.id = vim.api.nvim_open_win(self.buf, enter, self.config)
 end
 
 return Window
